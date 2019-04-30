c239f38794d278de8833b2038561c688e89ddc69
Darbestor <fordarbe@gmail.com>
2019-04-30 20:10:45 +0300

    fdf

--- d01/ex12/search_it!.php
+++ d01/ex12/search_it!.php
@@ -7,14 +7,10 @@
 		for ($i = 2; $i < $argc; $i++)
 		{
 			$arr[i - 2] = explode(":", $argv[$i]);
-			echo $arr[i - 2][0] . "\n";
-			echo $argv[1] . "\n\n";
-			if (($arr[i - 2][0]) == argv[1])
-			{
-				echo "true";
+			if (($arr[i - 2][0]) == $argv[1])
 				$value = $arr[i - 2][1];
-			}
 		}
-		//echo $value . "\n";
+		if ($value)
+			echo $value . "\n";
 	}
 ?>
